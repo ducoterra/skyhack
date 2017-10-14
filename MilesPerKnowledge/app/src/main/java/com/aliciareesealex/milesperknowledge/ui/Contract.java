4d161fa41f7111c516c9039bce8178f1e78a3929
Alex Oladele <dragid10@gmail.com>
2017-10-14 18:45:58 -0400

    Updates Classes

---
+++ MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/ui/Contract.java
@@ -0,0 +1,11 @@
+package com.aliciareesealex.milesperknowledge.ui;
+
+/**
+ * Name: Alex Oladele
+ * Unique-ID: OLADELAA
+ * Date: 10/14/17
+ * Project: MilesPerKnowledge
+ */
+
+public interface Contract {
+}
