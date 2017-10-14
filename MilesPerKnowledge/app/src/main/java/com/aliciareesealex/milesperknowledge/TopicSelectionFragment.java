a046afe74c1a1b4826a02eb4ab85000a28d1abc2
Alex Oladele <dragid10@gmail.com>
2017-10-14 16:40:21 -0400

    Renames CourseView to Topic Selection

--- MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/TopicSelectionFragment.java
+++ MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/TopicSelectionFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SubjectViewFragment extends Fragment {
-    static String TAG = SubjectViewFragment.class.getCanonicalName();
+public class TopicSelectionFragment extends Fragment {
+    static String TAG = TopicSelectionFragment.class.getCanonicalName();
 
-    public SubjectViewFragment() {
+    public TopicSelectionFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class SubjectViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subject_view, container, false);
+        return inflater.inflate(R.layout.fragment_topic_selection, container, false);
     }
 
 }
