0a5dc6c4d1c203116da932d34dd4ef4d9499dea7
ducoterra <ducoterra@gmail.com>
2017-10-14 19:18:01 -0400

    updates bridge.py to fetch from sql server properly

--- mpk_webapp/learn/bridge.py
+++ mpk_webapp/learn/bridge.py
@@ -4,4 +4,22 @@ Copyright 2017
 
 Is the bridge between views and the sql database
 '''
-import PyMySql
+import pymysql
+
+# Connect to the database
+connection = pymysql.connect(host='18.216.33.2',
+                             user='skyhack',
+                             password='eacss',
+                             db='mpkdb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `AirportCode` FROM `Airport` WHERE `City`=%s"
+        cursor.execute(sql, ('Charlotte',))
+        result = cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
