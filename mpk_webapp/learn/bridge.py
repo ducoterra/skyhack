85bfa9a3d769b69ff02c93f0c721b726f968334c
ducoterra <ducoterra@gmail.com>
2017-10-14 19:32:58 -0400

    updates bridge to fetch airport code and city from db

--- mpk_webapp/learn/bridge.py
+++ mpk_webapp/learn/bridge.py
@@ -6,20 +6,38 @@ Is the bridge between views and the sql database
 '''
 import pymysql
 
-# Connect to the database
-connection = pymysql.connect(host='18.216.33.2',
-                             user='skyhack',
-                             password='eacss',
-                             db='mpkdb',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+class Connection:
+    connection = pymysql.connect(host='18.216.33.2',
+                                 user='skyhack',
+                                 password='eacss',
+                                 db='mpkdb',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
 
-try:
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `AirportCode` FROM `Airport` WHERE `City`=%s"
-        cursor.execute(sql, ('Charlotte',))
-        result = cursor.fetchall()
-        print(result)
-finally:
+def cityToAirport(city):
+    connection = Connection.connection
+    try:
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT `AirportCode` FROM `Airport` WHERE `City`=%s"
+            cursor.execute(sql, (city,))
+            result = cursor.fetchone()
+            return result['AirportCode']
+    except:
+        return None
+
+def airportToCity(airport):
+    connection = Connection.connection
+    try:
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT `City` FROM `Airport` WHERE `AirportCode`=%s"
+            cursor.execute(sql, (airport,))
+            result = cursor.fetchone()
+            return result['City']
+    except:
+        return None
+
+def close():
+    connection = Connection.connection
     connection.close()
