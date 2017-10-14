56a45c8bce5dadd06d81e14093e2b946d4967e06
ducoterra <ducoterra@gmail.com>
2017-10-14 12:10:03 -0400

    getFlightInfo now returns the flight time between airports

--- mpk_webapp/learn/flightaware.py
+++ mpk_webapp/learn/flightaware.py
@@ -4,18 +4,43 @@ Copyright 2017
 
 Contains the methods for dealing with flightaware api data
 '''
-import requests
+import requests, time
 
 username = "ducoterra"
 apiKey = "5602c42a00d7b60e21ee8af46c59ec72daec436c"
 fxmlUrl = "https://flightxml.flightaware.com/json/FlightXML3/"
 
-payload = {'airport_code':'CLE', 'type':'enroute',
-    'howMany':'1'}
-response = requests.get(fxmlUrl + "AirportBoards",
-    params=payload, auth=(username, apiKey))
+def getFlightInfo(startCity = None, endCity = None,
+                    startId = None, endId = None):
+    '''
+    takes a startCity and endCity
+    -or-
+    startId and endId
 
-if response.status_code == 200:
-    print(response.json())
-else:
-    print("Error executing request")
+    returns the flight time in minutes
+    '''
+    # calculate times
+    now = int(time.time())
+    nextWeek = now + 604800
+    lastWeek = now - 604800
+
+    # api payload
+    payload = {'start_date' : lastWeek,
+                'end_date' : nextWeek,
+                'origin' : startId,
+                'destination' : endId,
+                'howMany' : 1}
+
+    # get request for data
+    response = requests.get(fxmlUrl + "AirlineFlightSchedules",
+        params=payload, auth=(username, apiKey))
+
+    if response.status_code == 200:
+        # parse through json
+        data = response.json()['AirlineFlightSchedulesResult']
+        flightInfo = data['flights'][0]
+        flightTime = flightInfo['arrivaltime'] - flightInfo['departuretime']
+        return flightTime // 60 # to minutes
+    else:
+        # if None is returned, no flighs found
+        return None
