460f50618d60619f5db7f6ff10e7b978fec20fea
ducoterra <ducoterra@gmail.com>
2017-10-14 01:15:15 -0400

    adds api example to flightaware.py and adds api_call_example.txt

--- mpk_webapp/learn/flightaware.py
+++ mpk_webapp/learn/flightaware.py
@@ -3,4 +3,19 @@ Author: Reese Wells
 Copyright 2017
 
 Contains the methods for dealing with flightaware api data
-''''
+'''
+import requests
+
+username = "ducoterra"
+apiKey = "5602c42a00d7b60e21ee8af46c59ec72daec436c"
+fxmlUrl = "https://flightxml.flightaware.com/json/FlightXML3/"
+
+payload = {'airport_code':'KSFO', 'type':'enroute',
+    'howMany':'10'}
+response = requests.get(fxmlUrl + "AirportBoards",
+    params=payload, auth=(username, apiKey))
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print("Error executing request")
