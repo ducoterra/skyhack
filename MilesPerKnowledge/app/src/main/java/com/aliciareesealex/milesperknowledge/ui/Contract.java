cbcb6e016d801a679a07b2012c7d0c3f2df2c3f2
Alex Oladele <dragid10@gmail.com>
2017-10-14 19:27:54 -0400

    Fully implement MVP Contract

--- MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/ui/Contract.java
+++ MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/ui/Contract.java
@@ -8,8 +8,86 @@ package com.aliciareesealex.milesperknowledge.ui;
  */
 
 public interface Contract {
-	void showToastMessage(String message);
 	interface MPKView {
-//		interface
+		//		void showToastMessage(String message);
+		interface TopicActivityView extends MPKView {
+
+		}
+
+		interface LoginActivityView extends MPKView {
+
+		}
+
+		interface MainActivityView extends MPKView {
+
+		}
+
+		interface SearchAirportFragmentView extends MPKView {
+
+		}
+
+		interface SearchCityFragmentView extends MPKView {
+
+		}
+
+		interface TopicSelectionFragmentView extends MPKView {
+
+		}
+
+		interface VideoFragmentView extends MPKView {
+
+		}
+	}
+
+	interface MPKPresenter {
+		interface TopicActivityPresenter extends MPKPresenter {
+
+		}
+
+		interface LoginActivityPresenter extends MPKPresenter {
+
+		}
+
+		interface MainActivityPresenter extends MPKPresenter {
+
+		}
+
+		interface SearchAirportFragmentPresenter extends MPKPresenter {
+
+		}
+
+		interface SearchCityFragmentPresenter extends MPKPresenter {
+
+		}
+
+		interface TopicSelectionFragmentPresenter extends MPKPresenter {
+
+		}
+
+		interface VideoFragmentPresenter extends MPKPresenter {
+
+		}
+	}
+
+	interface MPKModel {
+		interface Achievement extends MPKModel {
+
+		}
+
+		interface Airport extends MPKModel {
+
+		}
+
+		interface Flight extends MPKModel {
+
+		}
+
+		interface Topic extends MPKModel {
+
+		}
+
+		interface User extends MPKModel {
+
+		}
 	}
 }
