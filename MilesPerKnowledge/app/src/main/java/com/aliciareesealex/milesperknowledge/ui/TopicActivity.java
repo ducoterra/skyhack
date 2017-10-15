ac2233ba114ccfd833937875b411e9fa87a1434a
Alex Oladele <dragid10@gmail.com>
2017-10-14 23:00:46 -0400

    Ensures that Mysql connection is closed before exiting

--- MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/ui/TopicActivity.java
+++ MilesPerKnowledge/app/src/main/java/com/aliciareesealex/milesperknowledge/ui/TopicActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.aliciareesealex.milesperknowledge.Contract;
 import com.aliciareesealex.milesperknowledge.R;
+import com.aliciareesealex.milesperknowledge.util.MySqlDB;
 
 public class TopicActivity extends AppCompatActivity implements Contract.MPKView.TopicActivityView {
     static String TAG = TopicActivity.class.getCanonicalName();
@@ -13,4 +14,11 @@ public class TopicActivity extends AppCompatActivity implements Contract.MPKView
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
     }
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		MySqlDB sqlDB = new MySqlDB();
+		sqlDB.closeConnectionToDatabase();
+	}
 }
